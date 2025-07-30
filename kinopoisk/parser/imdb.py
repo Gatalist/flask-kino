d3479dd731ef22df2a3910156e8d9cdbaf2817d4
Gatalist <kostencko.alexander2012@gmail.com>
2025-07-30 15:51:19 +0300

    fix frontend html

--- kinopoisk/parser/imdb.py
+++ kinopoisk/parser/imdb.py
@@ -13,34 +13,6 @@ class IMDBMovie(WebRequester):
         self.film_imdb_url = f"{self.base_imdb_url}title/"
         self.pattern = re.compile(r'https://.*?\.jpg')
 
-    # def get_movie_photos(self, imdb_id) -> dict:
-    #     logger.info(f'\n----------- IMDB parsing ----------')
-    #     header = self.get_user_agent()
-    #
-    #     parse_url = f"{self.film_imdb_url}{imdb_id}"
-    #     screenshot_request_data = self.request_data(parse_url, header)
-    #
-    #     if screenshot_request_data["data"]:
-    #         screenshot = []
-    #
-    #         soup = bs(screenshot_request_data["data"].text, 'html.parser')
-    #         find_class = "ipc-shoveler ipc-shoveler--base ipc-shoveler--page0"
-    #         div_tags = soup.find('div', class_=find_class)
-    #
-    #         if div_tags:
-    #             for img_tag in div_tags.find_all('img', class_='ipc-image'):
-    #                 img_url = img_tag.get('srcset')
-    #                 if img_url:
-    #                     matches = self.pattern.findall(img_url)
-    #                     screenshot.append(matches[-1])
-    #
-    #         #  возвращаем картинки
-    #         screenshot_request_data["data"] = list(set(screenshot))
-    #         return screenshot_request_data
-    #
-    #     screenshot_request_data["data"] = {}
-    #     return screenshot_request_data
-
     def get_movie_photos(self, imdb_id) -> dict:
         logger.info(f'\n----------- IMDB parsing ----------')
         header = self.get_user_agent()
@@ -62,7 +34,7 @@ class IMDBMovie(WebRequester):
                         height = int(a.get('height'))
                         width = int(a.get('width'))
                         _coef = height / width
-                        if 0.4 < _coef < 1.2:
+                        if height < width and _coef < 1.2:
                             screenshot.append(img.get('src'))
 
             _images = list(set(screenshot))
